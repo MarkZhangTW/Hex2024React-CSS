f6860d25e4557f6b1ddf38ac1941b870f6bd8e46
Mark Zhang <markzhang1994@gmail.com>
2024-08-10 10:12:07 +0800

    Update series page main content.

--- task-week3/src/main.ts
+++ task-week3/src/main.ts
@@ -9,3 +9,33 @@ let samePageLinks = document.getElementsByClassName(samePageLinkClassName);
 for (let link of samePageLinks) {
     link.setAttribute('href', '#');
 }
+
+// Activate selection for series page.
+let firstSelection = document.querySelector('.selection');
+let firstBanner = document.querySelector('.banner');
+let firstItems = document.querySelector('.items');
+firstSelection?.classList.add('active');
+firstBanner?.classList.add('active');
+firstItems?.classList.add('active');
+
+// Add switch selection for series page.
+let selectionOnClick = (ev: MouseEvent) => {
+    // Deactivate all selections, banners, and items.
+    let selections: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('a.selection');
+    let banners: NodeListOf<HTMLDivElement> = document.querySelectorAll('div.banner');
+    let itemsAll: NodeListOf<HTMLDivElement> = document.querySelectorAll('div.items');
+    selections?.forEach(tag => tag.classList.remove('active'));
+    banners.forEach(tag => tag.classList.remove('active'));
+    itemsAll.forEach(tag => tag.classList.remove('active'));
+    // Activate selected selection and banner.
+    if (ev.target instanceof HTMLAnchorElement) {
+        let selection: HTMLAnchorElement = ev.target;
+        let banner = document.getElementById(ev.target.id.replace('selection', 'banner'));
+        let items = document.getElementById(ev.target.id.replace('selection', 'items'));
+        selection?.classList.add('active');
+        banner?.classList.add('active');
+        items?.classList.add('active');
+    }
+}
+let selectionLink: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('a.selection');
+selectionLink.forEach(tag => tag.onclick = selectionOnClick);
